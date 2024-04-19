047c9c0fcda8c1422e8da2f89a0753e68e32da0e
동원 <ai06021@gmail.com>
2024-04-19 16:31:40 +0900

    refactor : RandomBoolean interface create

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -5,14 +5,15 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
-public class Line {
-    private List<Boolean> list = new ArrayList<>();
+public class Line implements RandomBoolean{
+    private List<Boolean> connection = new ArrayList<>();
+    private Random random;
 
     public Line(int size) {
         basicLine(size);
         IntStream.range(1, size)
-                .filter(i -> !list.get(i - 1))
-                .forEach(i -> list.set(i, randomBoolean()));
+                .filter(i -> !connection.get(i - 1))
+                .forEach(i -> connection.set(i, randomBoolean()));
     }
 
     private boolean randomBoolean() {
@@ -21,11 +22,16 @@ public class Line {
 
     private void basicLine(int k) {
         for (int i = 0; i < k; i++) {
-            list.add(false);
+            connection.add(false);
         }
     }
 
     public List<Boolean> getList() {
-        return list;
+        return connection;
+    }
+
+    @Override
+    public boolean Boolean() {
+        return random.nextBoolean();
     }
 }
